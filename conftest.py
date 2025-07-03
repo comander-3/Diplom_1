7e8c4383b976c0ca870a48aeb75df22516193a00
comander-3 <comander_3@mail.ru>
2025-07-04 02:08:02 +0500

    add tests for class: Burger, Bun, Ingredient; add conftest

--- conftest.py
+++ conftest.py
@@ -1,17 +1,25 @@
 import pytest
 from unittest.mock import Mock
 
-
 @pytest.fixture
-def bun_mock():
+def black_bun_mock():
     mock_bun = Mock()
-    mock_bun.name = None
-    mock_bun.price = None
+    mock_bun.name = "black bun"
+    mock_bun.price = 100
     return mock_bun
 
 @pytest.fixture
-def ingredients_mock():
+def ingredient_hot_sause_mock():
     mock_ingredient = Mock()
-    mock_ingredient.name = None
-    mock_ingredient.price = None
+    mock_ingredient.type = 'SAUCE'
+    mock_ingredient.name = "hot sauce"
+    mock_ingredient.price = 100
+    return mock_ingredient
+
+@pytest.fixture
+def ingredient_cutlet_mock():
+    mock_ingredient = Mock()
+    mock_ingredient.type = 'FILLING'
+    mock_ingredient.name = "cutlet"
+    mock_ingredient.price = 100
     return mock_ingredient
